b2f69caeac8fda07094a64ba508f478b6b4cc49e
Pavle Radosavljevic <pavlepiramida@gmail.com>
2017-09-29 17:24:26 +0200

    Hahah i suck at cleaning and pushing stuff

--- cm.mk
+++ cm.mk
@@ -1,5 +1,3 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/kylessopen/kylessopen.mk)
